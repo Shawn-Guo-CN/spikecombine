856b42de255681c04dd163f02bf2079cc80b09ad
Shawn Guo <shawnguo.cn@gmail.com>
2019-10-30 11:04:26 +0000

    updated fit function

--- spikesortercombiner.py
+++ spikesortercombiner.py
@@ -1,44 +1,83 @@
 import pickle
+import numpy as np
 
 class SpikeSortersCombiner(object):
     """
     This is the model for combining results from different sorters. 
 
     Assume that we want to combine K different sorters, then the key parameters are given as follows:
-    1. K mean vectors and the corresponding K covariance matrix;
+    1. K mean vectors (1*N) and the corresponding K covariance matrix (N*N);
     2. categorical distribution on K different sorters.
     """
     def __init__(self):
         self._sorter_names = None
 
         # the following 3 are parameters
-        self._means = None
-        self._covars = None
-        self._sorter_prior = None
-        
-        self._name_to_mean = None
-        self._name_to_covar = None
-        self._name_to_sorter_prior = None
+        self.params = {
+            'positive':{
+                'means': None, # should be a [K, N] np array
+                'covars': None, # should be a [K, N, N] np array
+                'sorter_prior': None, # should be a [1, K] np array
+            },
+            'negative':{
+                'means': None, # should be a [K, N] np array
+                'covars': None, # should be a [K, N, N] np array
+                'sorter_prior': None, # should be a [1, K] np array
+            }
+        }
 
-    def _filter_out_wrong_data(self):
+        self._sortername_to_id = None
+
+    def _generate_data_for_one_sorter(self):
         raise NotImplementedError
 
-    def _generate_data_matrix_for_one_sorter(self):
+    def _fit_one_sorter(self, data):
         raise NotImplementedError
 
-    def fit(self, dataloader):
-        gt_sortings = dataloader.gt_sortings
-        sorter_sortings = dataloader.sorter_sortings
+    @staticmethod
+    def _set_prior(K, mode:str='uniform'):
+        if mode == 'uniform':
+            return np.ones(K, dtype=float) / float(K)
+        else:
+            rasie NotImplementedError
 
-        self._sorter_names = sorter_sortings.keys()
+    def fit(self, dataloader, prior_mode:str='uniform') -> None:
+        K = len(dataloader.sorter_names)
 
-        raise NotImplementedError
+        _positive_means = []
+        _positive_covars = []
+        _positive_prior = self._set_prior(K, prior_mode)
+
+        _negative_means = []
+        _negative_covars = []
+        _negative_prior = self._set_prior(K, prior_mode)
+
+        for sorter_name in dataloader.sorter_names():
+            positive_data, negative_data = self._generate_data_for_one_sorter(dataloader.sorter_sortings[sorter_name])
+
+            pos_mean, pos_covar = self._fit_one_sorter(positive_data)
+            _positive_means.append(pos_mean)
+            _positive_covars.append(pos_covar)
+
+            neg_mean, neg_covar = self._fit_one_sorter(negative_data)
+            _negative_means.append(neg_mean)
+            _negative_covars.append(neg_covar)
+
+            self._sortername_to_id[sorter_name] = len(_positive_means) - 1
 
     def predict(self):
-        assert self.means is not None and self.covars is not None and self.sorter_prior is not None, """
+        assert all(), """
             Please fit or load model before prediction.
         """
         raise NotImplementedError
+
+    def get_params_for_sorter(self, sortername:str=None):
+        return {
+            'positive_mean': self.params['positive']['means'][self._sortername_to_id[sortername]],
+            'positive_covar': self.params['positive']['covars'][self._sortername_to_id[sortername]],
+            'negative_mean': self.params['negative']['means'][self._sortername_to_id[sortername]],
+            'negative_covar': self.params['negative']['covars'][self._sortername_to_id[sortername]],
+        }
 
     def load(self):
         raise NotImplementedError
