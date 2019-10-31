8d8ad5052efa466b767968a534866e3affb389b0
Shawn Guo <shawnguo.cn@gmail.com>
2019-10-31 16:42:02 +0000

    finished _generate_data_for_one_sorter function

--- spikesortercombiner.py
+++ spikesortercombiner.py
@@ -1,6 +1,9 @@
 import pickle
 import numpy as np
 
+import spikeinterface.comparison as sc
+import spikeinterface.toolkit as st
+
 class SpikeSortersCombiner(object):
     """
     This is the model for combining results from different sorters. 
@@ -9,7 +12,7 @@ class SpikeSortersCombiner(object):
     1. K mean vectors (1*N) and the corresponding K covariance matrix (N*N);
     2. categorical distribution on K different sorters.
     """
-    def __init__(self):
+    def __init__(self, well):
         self._sorter_names = None
 
         # the following 3 are parameters
@@ -28,8 +31,39 @@ class SpikeSortersCombiner(object):
 
         self._sortername_to_id = None
 
-    def _generate_data_for_one_sorter(self):
-        raise NotImplementedError
+    def _generate_data_for_one_sorter(self, sorter_sortings, gt_sortings, recordings):
+        """
+        Generate positive and negative data matrices based on the sorter's results and ground truth results.
+        """
+        assert sorter_sortings.keys() == gt_sortings.keys(), """
+            The dataset stored in ground truth results and sorters' results are different.
+        """
+
+        positive_metrics = []
+        negative_metrics = []
+
+        for dataset_name in sorter_sortings.keys():
+            sorting_gt = gt_sortings[dataset_name]
+            sorting_sorter = sorter_sortings[dataset_name]
+            recording = recordings[dataset_name]
+
+            gt_comparison = sc.compare_sorter_to_ground_truth(sorting_gt, sorting_sorter, exhaustive_gt=True)
+            well_detected_units = gt_comparison.get_well_detected_units()
+
+            metric_matrix = st.validation.MetricCalculator(sorting_sorter, recording)
+            metric_matrix.compute_metrics()
+            metrics_df = metric_matrix.get_metrics_df()
+
+            for unit_id in sorting_sorter.get_unit_ids():
+                if unit_id in well_detected_units:
+                    positive_metrics.append(metrics_df.loc[unit_id, :].values)
+                else:
+                    negative_metrics.append(metrics_df.loc[unit_id, :].values)
+
+        positive_metrics = np.array(positive_metrics)
+        negative_metrics = np.array(negative_metrics)
+        
+        return positive_metrics, negative_metrics
 
     def _fit_one_sorter(self, data):
         raise NotImplementedError
@@ -39,7 +73,7 @@ class SpikeSortersCombiner(object):
         if mode == 'uniform':
             return np.ones(K, dtype=float) / float(K)
         else:
-            rasie NotImplementedError
+            raise NotImplementedError
 
     def fit(self, dataloader, prior_mode:str='uniform') -> None:
         K = len(dataloader.sorter_names)
@@ -53,7 +87,11 @@ class SpikeSortersCombiner(object):
         _negative_prior = self._set_prior(K, prior_mode)
 
         for sorter_name in dataloader.sorter_names():
-            positive_data, negative_data = self._generate_data_for_one_sorter(dataloader.sorter_sortings[sorter_name])
+            positive_data, negative_data = self._generate_data_for_one_sorter(
+                    dataloader.gt_sortings[sorter_name],
+                    dataloader.sorter_sortings[sorter_name],
+                    dataloader.recordings[sorter_name]
+                )
 
             pos_mean, pos_covar = self._fit_one_sorter(positive_data)
             _positive_means.append(pos_mean)
